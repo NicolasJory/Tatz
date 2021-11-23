0f8d9c0613cffc187546a05550bda11aa91219c8
Nicolas <nicolas.jory.prog@gmail.com>
2021-11-23 17:40:34 +0100

    Profils page part 1

--- screens/Profils/Profils.js
+++ screens/Profils/Profils.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, Button } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-ionicons';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 export default class Caroussel extends React.Component {
     render(){
         return (
-          <View>
-          <Text>{this.props.route.params.params.artist}</Text>
-          <Text>{this.props.route.params.params.studio}</Text>
-          <Image source={this.props.route.params.params.pp}/>
+          <View style={styles.container}>
+            <ScrollView stickyHeaderIndices={[1]} style={{borderBottomWidth: 1,borderBottomColor:"rgba(96, 44, 201, 1)"}}>
+              <View style={styles.header}>
+                <View style={styles.title}>
+                  <Image style={styles.pp} source={this.props.route.params.params.pp}/>
+                  <View style={styles.titleText}>
+                    <Text style={styles.artist}>{this.props.route.params.params.artist}</Text>
+                    <Text style={styles.studio}>{this.props.route.params.params.studio}</Text>
+                  </View> 
+                  <View style={styles.button}>
+                    <Button color= 'rgba(96, 44, 201, 1)' title="follow">Follow</Button> 
+                  </View>
+                </View>
+                <View style={styles.description}>
+                  <Text>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…). ... L'étude formelle des textes s'appuie sur la linguistique, qui est l'approche scientifique du langage</Text>
+                </View>
+              </View>
+              <View style={styles.tabNav}>
+                <Icon></Icon>
+              </View>
+            </ScrollView>
           </View>
           )
     }
@@ -17,5 +38,57 @@ export default class Caroussel extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+      flex:1,
+      paddingBottom: (width*0.125),
+      backgroundColor: 'rgb(204, 179, 255)',
+    },
+    header:{
+      height: height*0.33,
+      borderBottomWidth:1,
+      borderTopWidth:1,
+      borderBottomColor: 'rgba(96, 44, 201, 1)',
+      borderTopColor: 'rgba(96, 44, 201, 1)',
+    },
+    title:{
+      flex:1,
+      flexWrap: 'wrap', 
+      flexDirection:'row',
+    },
+    pp:{
+      height: width/5,
+      width: width/5,
+      borderRadius:width/5,
+      resizeMode: 'cover',
+      borderColor: 'rgba(96, 44, 201, 1)',
+      borderWidth:2,
+      margin: width/18,
+    },
+    titleText:{
+      justifyContent: 'center',
+      borderBottomWidth: 1,
+      borderBottomColor: 'rgba(96, 44, 201, 1)'
+    },
+    artist:{
+      fontSize: width/20,
+      fontFamily: 'rooters',
+    },
+    studio:{
+      fontSize: width/25,
+      marginTop: width/25,
+    },
+    button:{
+      justifyContent: 'center',
+      marginLeft: width/20,
+    },
+    description:{
+      flex:1,
+      padding: width/30,
+    },
+    tabNav:{
+      flex:1,
+      flexWrap: 'wrap', 
+      flexDirection:'row',
+    }
+    
   });
